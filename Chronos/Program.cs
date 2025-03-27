994c5b5f42238e56099c148ab51018a20791478f
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-27 18:03:02 +0000

    Stop the counter and show elapsed time

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -27,6 +27,14 @@ namespace Chronos
             // Pause for 0.25 seconds
             Thread.Sleep(250);
             Console.WriteLine("Crono 2 paused for 250 milliseconds...");
+
+            // Stop both stopwatches
+            crono1.Stop();
+            crono2.Stop();
+
+            // Display elapsed time for both stopwatches in seconds with 4 decimal places
+            Console.WriteLine($"Crono 1 elapsed time: {crono1.Elapsed.TotalSeconds:F4} seconds");
+            Console.WriteLine($"Crono 2 elapsed time: {crono2.Elapsed.TotalSeconds:F4} seconds");
         }
     }
 }
