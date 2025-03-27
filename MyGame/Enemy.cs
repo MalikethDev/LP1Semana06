53ae9615403477b69ceac41bf5b73a86f30a081f
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-27 20:48:01 +0000

    Add enemy class

---
+++ MyGame/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Enemy
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enemy");
+        }
+    }
+}
