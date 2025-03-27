62a9396c4e7f199467b06f621679b7919eac84a4
Rodrigo <rodrigogomes091997@gmail.com>
2025-03-27 17:47:24 +0000

    Declare chrono1 and chrono2

---
+++ Chronos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch chrono1 = new Stopwatch();
+            Stopwatch chrono2 = new Stopwatch();
+        }
+    }
+}
